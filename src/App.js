55ac7273c24706be85a8ecbcd7fe59204165aeb2
manikanta9578 <manikanta9578@gmail.com>
2022-12-28 12:25:07 +0530

    redux_increment/decrement counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { incNum, decNum } from './actions'
 
-function App() {
+
+const App = () => {
+
+  const count = useSelector(state => state.changeTheNum);
+  const dispatch = useDispatch(); 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{count}</h1>
+      <button onClick={() => {dispatch(incNum())}}>+</button>
+      <button onClick={() => {dispatch(decNum())}}>-</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from "redux";
+import changeTheNum from "./incDec";
+
+const reducers = combineReducers(
+    {
+        changeTheNum
+    }
+)
+
+export default reducers;
